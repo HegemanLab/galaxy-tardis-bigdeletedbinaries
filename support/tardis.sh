d41064cbc4a32f6830608c2e1c2825011424c304
Art Eschenlauer <esch0041@umn.edu>
2019-04-11 20:51:05 -0500

    reformatted for test backup

--- support/tardis.sh
+++ support/tardis.sh
@@ -14,6 +14,15 @@ if [ $# -lt 1 ]; then
   exit 0
 fi
 
+# set the actual script directory per https://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
 subcommand=$1
 shift # Remove first argument from the argument list
 
@@ -31,7 +40,7 @@ case "$subcommand" in
   transmit)
     echo ---
     echo `date --iso-8601=seconds` Transmit starting
-    echo "WARNING 'transmit' not yet implemented."
+    bash $DIR/transmit_backup.sh
     echo `date --iso-8601=seconds` Transmit ended
     echo ...
   ;;
