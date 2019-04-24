d959eeaeff50533f88819e9389ab600d758adb49
Art Eschenlauer <eschen@alumni.princeton.edu>
2019-04-24 12:10:40 -0500

    migrate changes from detached repo

--- s3/bucket_backup.sh
+++ s3/bucket_backup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # arg 1, required - file to backup
-# arg 2, optionao - suffix added for object store, typically some portion of the current date and time
+# arg 2, optional - prefix added to absolute path in bucket (root subdirectory path in bucket)
+# arg 3, optional - suffix added for object store, typically some portion of the current date and time
 # NOTE WELL - This script ASSUMES that it is located in export/s3
 
 # set the actual script directory per https://stackoverflow.com/a/246128
@@ -16,17 +17,21 @@ DIR="$( cd -P "$( dirname "${SOURCE}" )" >/dev/null 2>&1 && pwd )"
 # set EXPORT_ROOT and CONFIG_BUCKET
 source ${DIR}/dest.config
 
-if [ $# -le 2 -a -e $1 ]; then
+if [ $# -le 3 -a -e $1 ]; then
   if [[ "${1:0:1}" = "/" ]]; then
     ABSOLUTE=$1
   else
     ABSOLUTE=$( cd $(dirname $1) && pwd -P)/$(basename $1)
   fi
-  SUFFIX=""
+  PREFIX=""
   if [ ! -z "$2" ]; then
-    SUFFIX="-$2"
+    PREFIX="/$2"
   fi
-  s3cmd --no-mime-magic -c ${DIR}/dest.s3cfg sync ${ABSOLUTE} s3://${CONFIG_BUCKET}${ABSOLUTE}${SUFFIX}
+  SUFFIX=""
+  if [ ! -z "$3" ]; then
+    SUFFIX="-$3"
+  fi
+  s3cmd --no-mime-magic -c ${DIR}/dest.s3cfg sync ${ABSOLUTE} s3://${CONFIG_BUCKET}${PREFIX}${ABSOLUTE}${SUFFIX}
 else
   echo 'usage: bucket_backup.sh path_to_a_file [suffix]'
 fi
