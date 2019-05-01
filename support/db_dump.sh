e441ed967072585bb385c5b7acf7c888391bf6c7
Art Eschenlauer <eschen@alumni.princeton.edu>
2019-05-01 14:32:33 -0500

    restore repositories when required

--- support/db_dump.sh
+++ support/db_dump.sh
@@ -5,13 +5,28 @@ CVS=/export/support/cvs
 # set path to main postgresql database only when it is not already set
 PGDATA=${PGDATA:-/var/lib/postgresql/data}
 
+# ensure that path /export/backup exists and is owned by galaxy
+if [ ! -d /export/backup ]; then
+  if [ -d /export/restore/export/backup ]; then
+    mv /export/restore/export/backup /export/backup
+  else
+    mkdir -p /export/backup
+    chown 1450:1450 /export/backup
+  fi
+fi
+
 # ensure that path /export/backup/pg exists and is owned by postgres
-if [ ! -d /export/backup ]; then mkdir /export/backup; fi
-if [ ! -d /export/backup/pg ]; then mkdir /export/backup/pg; fi
-cd /export/backup/pg
-chown postgres .
+if [ ! -d /export/backup/pg ]; then
+  if [ -d /export/restore/export/backup/pg ]; then
+    mv /export/restore/export/backup/pg /export/backup/pg
+  else
+    mkdir -p /export/backup/pg
+    chown postgres /export/backup/pg
+  fi
+fi
 
 # init CVS repository at /export/backup/pg, owned by postgres
+cd /export/backup/pg
 if [ ! -d CVSROOT ]; then su -l -c "$CVS -d /export/backup/pg init" postgres; fi
 if [ ! -d dumpall ]; then su -l -c 'mkdir /export/backup/pg/dumpall' postgres; fi
 
