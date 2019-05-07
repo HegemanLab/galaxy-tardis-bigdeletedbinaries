8ec2af4be2c13c64bbebe7b266e232621691a333
Art Eschenlauer <eschen@alumni.princeton.edu>
2019-05-06 20:46:07 -0500

    working better, but needs a bit more work

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #  The `tar` command -h  options dereference logical links when instantiating the build environment,
 #    (inspired by https://github.com/moby/moby/issues/18789#issuecomment-165985865).
+set -e
+chmod +x support/busybox-static
 tar ch . | docker build -t tardis -
+chmod -x support/busybox-static
