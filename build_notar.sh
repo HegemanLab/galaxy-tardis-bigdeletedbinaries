0ebab1048173c169a9ac81cb951db4563dc9d6d6
Art Eschenlauer <eschen@alumni.princeton.edu>
2019-05-20 15:32:47 -0500

    pull down docker-usernetes.gz if support/docker-usernetes does not exist

--- build_notar.sh
+++ build_notar.sh
@@ -1,4 +1,10 @@
 set -e
+if [ ! -f support/docker-usernetes ]; then
+  pushd support
+  wget https://github.com/HegemanLab/galaxy-tardis/releases/download/v0.1.0/docker-usernetes.gz --output-document docker-usernetes.gz
+  gzip -d docker-usernetes.gz
+  popd
+fi
 chmod +x support/busybox-static
 chmod +x support/docker-usernetes
 docker build -t tardis .
